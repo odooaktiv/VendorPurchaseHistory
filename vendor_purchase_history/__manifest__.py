fe79eee9dd1fd9779399fa8348211295a51232db
odooaktiv <odoo@aktivsoftware.com>
2019-08-13 16:25:51 +0530

    [ADD] Initial commit for branch 10.0

--- vendor_purchase_history/__manifest__.py
+++ vendor_purchase_history/__manifest__.py
@@ -20,7 +20,7 @@
     'license': "AGPL-3",
 
     'category': 'Purchases',
-    'version': '11.0.1.0.0',
+    'version': '10.0.1.0.0',
 
     # any module necessary for this one to work correctly
     'depends': ['purchase', 'product'],
